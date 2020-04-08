3aaa33443774359a3907ffa6ed315f9b6f35f0d2
gjj2828 <gjj2828@gmail.com>
2020-04-08 18:58:36 +0800

    Add C# sample.

---
+++ Params/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class MainClass
+{
+    public static void Main(string[] args) {
+        Print(1, 2, 3);
+    }
+
+    static void Print(params int[] args) {
+        Console.Write("Print:");
+        foreach(int arg in args) {
+            Console.Write(" {0}", arg);
+        }
+        Console.WriteLine();
+    }
+}
